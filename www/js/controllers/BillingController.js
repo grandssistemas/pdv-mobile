05deb3faec06402b5269109e2a1b6ac5bb05cdae
rafaelMangolin <rbmangolin@gmail.com>
2016-09-16 10:38:51 -0300

    Finalizado a integração com o serviço da redecard

--- www/js/controllers/BillingController.js
+++ www/js/controllers/BillingController.js
@@ -9,6 +9,7 @@ angular.module('app.core')
   $cordovaToast,
   $timeout){
   $scope.entity = entity;
+  $scope.paymentType = '';
   var paymentTypes;
   initializeEntity($scope.entity);
 
@@ -39,13 +40,13 @@ angular.module('app.core')
 
   $scope.update = update;
 
-  function update(entity){
+  function update(entity, paymentType){
     entity = angular.copy(entity);
     entity.isBilled = {value: true};
     entity.status = 'FINISH';
     entity.entries = [{
       value: entity.total,
-      paymentType: getPaymentType($scope.paymentType)
+      paymentType: getPaymentType(paymentType)
     }]
     entity.subTotal === 0 ? angular.noop : entity.subTotal *= -1;
     entity.total === 0 ? angular.noop : entity.total *= -1;
@@ -58,11 +59,28 @@ angular.module('app.core')
         return data;
     });
     entity.cashAccountMovements = [];
-    var pagtype = getTipoDePagamento($scope.paymentType);
+    var pagtype = getTipoDePagamento(paymentType);
     if(pagtype){
-      var valor = (entity.total*-100).toFixed(0);
-      ConectaRedeCard.lancaPagamento(valor,localStorageService.get('token'),1,function(resp){
-        doMovement(entity);
+      var valor = parseInt(entity.total*-100),
+          token = localStorageService.get('token');
+      ConectaRedeCard.lancaPagamento(valor,token,pagtype,1,function(resp){
+        resp = JSON.parse(resp);
+        switch (resp.status) {
+          case 'AUTHORIZED':
+          case 'COMPLETED':
+            doMovement(entity);
+            break;
+          case 'CANCELED':
+            $cordovaToast.showShortTop('Pagamento cancelado.');
+            break;
+          case 'FAILED':
+            $cordovaToast.showShortTop('Falha no pagamento.');
+            break;
+          case 'REFUNDED':
+            $cordovaToast.showShortTop('Pagamento extornado.');
+          default:
+
+        }
       },credError)
     }else{
       doMovement(entity);
@@ -75,8 +93,8 @@ angular.module('app.core')
       return 'CREDITO_A_VISTA';
     case 'Cartão de Débito':
       return 'DEBITO';
-      default:
-        return null;
+    default:
+      return null;
     }
   }
 
@@ -87,12 +105,12 @@ angular.module('app.core')
   function doMovement(entity){
     MovementGroupService.billing(entity).then(function(data){
       ShoppingCartService.setShoppingCart([]);
+      $cordovaToast.showShortTop('Venda concluida.');
       $state.go('menu.searchprod');
     })
   }
 
   function getPaymentType(paymentType){
-    console.log(paymentType)
     return paymentTypes.reduce(function(a,b){
       if(b.name.indexOf(paymentType)){
         return b;
